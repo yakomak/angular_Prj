b1cd1a62ae70fe07d07fba14ed00b1f18ad698b6
Maxim <yakomak137@gmail.com>
2017-03-30 23:18:14 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
-angular.module
-.controller
-.filter
-.directive
+// angular.module
+// .controller
+// .filter
+// .directive
+
+// Define the `phonecatApp` module
+var app = angular.module('App', []);
+
+// Define the `PhoneListController` controller on the `phonecatApp` module
+app.controller('PLController', function PLController($scope) {
+  $scope.phones = [
+    {
+      name: 'Nexus S',
+      snippet: 'Fast just got faster with Nexus S.'
+    }, {
+      name: 'Motorola XOOM™ with Wi-Fi',
+      snippet: 'The Next, Next Generation tablet.'
+    }, {
+      name: 'MOTOROLA XOOM™',
+      snippet: 'The Next, Next Generation tablet.'
+    }
+  ];
+});
